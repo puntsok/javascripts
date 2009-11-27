0990f6d2640314b7a8d7d815f03752581a7e7e46
Christopher Walker <cewalker@puntsok.local>
2009-11-27 08:53:56 -0500

    created more practice tests

--- bokug_tests.js
+++ bokug_tests.js
@@ -7,7 +7,7 @@ YUI().use(
 	C = new Y.Console({
 		height:            '650px',
 		width:             '500px',
-		collapsed:         true,
+		collapsed:         false,
 		/*logSource:         Y.Global,*/
 		useBrowserConsole: false,
 		newestOnTop:       false,
@@ -25,14 +25,44 @@ Y.use( 'test', function(Y) {
 
 	var testCase = new Y.Test.Case({
 		name: 'FirstTestCase',
+		setUp: function() {
+			this.data = 1;
+		},
+		
+		tearDown: function() {
+			// do some more
+		},
+		
 		'should show 1 + 1 is equal to 2': function() {
-			Y.Assert.areEqual( 1+1, 2 );		
+			Y.Assert.areEqual( this.data + 1, 2 );		
+		},
+		'this.data + 3 should be four': function() {
+			Y.Assert.areEqual( this.data + 3, 4 );
+		}
+	});
+	
+	var test2Case = new Y.Test.Case({
+		name: 'SecondTestCase',
+		'This should is my second test': function() {
+			Y.Assert.areEqual( 1+3, 4 );		
+		}
+	});
+	
+	var testCase3 = new Y.Test.Case({
+		name: 'ThirdCaseTest',
+		'should This is my third test': function() {
+			Y.Assert.areEqual( 2, 2 );		
 		}
 	});
 
-	Y.Test.Runner.add( testCase );
-	Y.Test.Runner.run();
+	var suite = new Y.Test.Suite("my suite");
+	suite.add( test2Case );
+	suite.add( testCase3 );
+	suite.add( testCase );
+	
 
+	Y.Test.Runner.add( suite ).run();
+	
 });
 
 // ********************************************************
